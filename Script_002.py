64ade74785371bcf4314e96b352c0bc6bf69eacb
Nermin Kuc <nerminkuc74@gmail.com>
2020-07-09 17:49:30 +1000

    Origin

--- Script_002.py
+++ Script_002.py
@@ -9,7 +9,7 @@ app = dash.Dash(__name__)
 app.layout = dash_table.DataTable(
     id='table',
     columns=[{"name": i, "id": i} for i in df.columns],
-    data=df.to_dict('records'),)
+    data=df.to_dict('reccords'),)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
